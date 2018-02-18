4f76bbcc1847adc84d2cf510496259399f2276ed
Adam Elaoud <elaoud.adam@gmail.com>
2018-02-18 06:36:52 -0500

    Completed load and save system

---
+++ src/data/DataStorage.java
@@ -0,0 +1,86 @@
+package data;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import entities.Card;
+import entities.Deck;
+
+public class DataStorage implements Serializable {
+
+	private static final long serialVersionUID = 1752536480676738644L;
+	
+	// Saving
+	FileOutputStream saveFile;
+	ObjectOutputStream save;
+	
+	// Loading
+	FileInputStream loadFile;
+	ObjectInputStream load;
+
+	public DataStorage() {
+		try {
+			// Saving
+			saveFile = new FileOutputStream("data\\saveFile.sav");
+			save = new ObjectOutputStream(saveFile);
+			
+			// Loading
+			loadFile = new FileInputStream("data\\saveFile.sav");
+			load = new ObjectInputStream(loadFile);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveObject(Object obj) {
+		try {
+			save.writeObject(obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Object loadObjects() {
+		try {
+			Object obj = load.readObject();
+			
+			// Deck cast
+			if (obj instanceof Deck) {
+				return (Deck) obj;
+				
+			// Card cast
+			} else if (obj instanceof Card) {
+				return (Card) obj;
+				
+			// Default
+			} else {
+				return obj;
+			}
+			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	public void closeSave() {
+		try {
+			// close Output stream
+			save.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeLoad() {
+		try {
+			// close Input stream
+			load.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
